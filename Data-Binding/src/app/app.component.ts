56f45af83f46089f91f1faac3d625bb672934701
DHANABALAN VENKITUSAMY <dhanabalanvk@gmail.com>
2021-12-01 11:40:26 -0800

    Data binding types

--- Data-Binding/src/app/app.component.ts
+++ Data-Binding/src/app/app.component.ts
@@ -3,20 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   //string interpolation
   title = 'Data-Binding';
   //property binding
-imageUrl="https://www.facebook.com/photo.php?fbid=2848283105263152&set=t.100003312101267&type=3";
+  imageUrl =
+    'https://www.facebook.com/photo.php?fbid=2848283105263152&set=t.100003312101267&type=3';
 
-
-  allowFunction=false;
-  constructor(){
+  allowFunction = false;
+  constructor() {
     setTimeout(() => {
-      this.allowFunction=true;
+      this.allowFunction = true;
     }, 5000);
   }
-  
+
+  //event binding
+  greet() {
+  alert("hello");
+  }
+  firstName:string="subha";
 }
