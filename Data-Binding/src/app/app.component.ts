b6672f309090084b82943be17b97349fe84cb7b3
DHANABALAN VENKITUSAMY <dhanabalanvk@gmail.com>
2021-11-30 18:15:16 -0800

    data binding _angular

---
+++ Data-Binding/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  //string interpolation
+  title = 'Data-Binding';
+  //property binding
+imageUrl="https://www.facebook.com/photo.php?fbid=2848283105263152&set=t.100003312101267&type=3";
+
+
+  allowFunction=false;
+  constructor(){
+    setTimeout(() => {
+      this.allowFunction=true;
+    }, 5000);
+  }
+  
+}
