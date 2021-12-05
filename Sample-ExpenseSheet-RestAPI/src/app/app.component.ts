79e71124e045bd7f5c3f9d6df80b6c179383455a
DHANABALAN VENKITUSAMY <dhanabalanvk@gmail.com>
2021-12-05 11:17:12 -0800

    adding form

--- Sample-ExpenseSheet-RestAPI/src/app/app.component.ts
+++ Sample-ExpenseSheet-RestAPI/src/app/app.component.ts
@@ -4,26 +4,23 @@ import { ProductService } from './product.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Sample-ExpenseSheet-RestAPI';
-  products:any=[];
-  constructor(private service:ProductService){
-    //this.product=this.service.getAllproducts().subscribe(data=>{
-      //this.product=data;//})
-      //this.product=this.service.getAllproducts();
-      //console.log(this.product);
-    }ngOnInit(): void {
-    this.products=this.service.getAllproducts().subscribe(data=>{
-      this.products=data;
+
+  products: any = []; //array
+  model: any = {}; //object created
+
+  constructor(private service: ProductService) {}
+
+  ngOnInit(): void {
+    this.products = this.service.getAllproducts().subscribe((data) => {
+      this.products = data;
       console.log(this.products);
-    }
-
-    )
+    });
   }
-;
-    
-
-  
+  buttonClickEvent() {
+    console.log('click event');
+  }
 }
--- Sample-ExpenseSheet-RestAPI/src/app/product.service.ts
+++ Sample-ExpenseSheet-RestAPI/src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { observable, Observable } from 'rxjs';
 
 
 @Injectable({
@@ -8,8 +9,8 @@ import {HttpClient} from '@angular/common/http';
 export class ProductService {
 
   constructor(private http: HttpClient) { }
-
-  getAllproducts(){
+//write one method (own) that return data from api
+  getAllproducts():Observable<any>{
     return this.http.get("http://localhost:3000/products/");
   }
 
